393b9435faf87dcc2af737c31fb81466724cd256
Miguel Breceda <mbreceda@contractroom.com>
2018-07-19 00:31:54 -0500

    challenge 5 Models

--- first_app/admin.py
+++ first_app/admin.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from first_app.models import AccessRecord, Topic, Webpage
+admin.site.register(AccessRecord)
+admin.site.register(Topic)
+admin.site.register(Webpage)
