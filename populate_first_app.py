f8900eb8c67f8feb660cd1c05d007ebe58b093a9
Miguel Breceda <miguel.breceda@outlook.es>
2018-08-12 22:07:48 -0500

    create populate script

---
+++ populate_first_app.py
@@ -0,0 +1,20 @@
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "first_project.settings")
+import django
+django.setup()
+
+from first_app.models import User
+from faker import Faker
+fakegen = Faker()
+
+
+def populate(N=5):
+    for entry in range(N):
+        fake_first_name = fakegen.first_name()
+        fake_last_name = fakegen.last_name()
+        fake_email = fakegen.email()
+        User.objects.get_or_create(first_name=fake_first_name, last_name=fake_first_name, email=fake_email)
+
+if __name__ == '__main__':
+    populate(20)
