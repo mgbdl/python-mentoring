49c2c446fd8b9174e5783e4fad724ebc32d1474b
Miguel Breceda <miguel.breceda@outlook.es>
2018-08-12 22:32:03 -0500

    update population script

--- populate_first_app.py
+++ populate_first_app.py
@@ -14,7 +14,7 @@ def populate(N=5):
         fake_first_name = fakegen.first_name()
         fake_last_name = fakegen.last_name()
         fake_email = fakegen.email()
-        User.objects.get_or_create(first_name=fake_first_name, last_name=fake_first_name, email=fake_email)
+        User.objects.get_or_create(first_name=fake_first_name, last_name=fake_last_name, email=fake_email)
 
 if __name__ == '__main__':
     populate(20)
